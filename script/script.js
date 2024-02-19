a8ee8df8e1d178e8880d50ed9054457b6d49b814
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-02-19 12:03:32 +0600

    number input field and next button enable/disable on cout is implimented

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
-
-let count =0;
+let count = 0;
 const price = 550;
 const NEW15 = 0.15;
 const Couple20 = 0.2;
@@ -12,110 +11,143 @@ const busClass = "Economoy";
 //     grandTotalCountCoupon(couponCode);
 // });
 
-const seats = document.querySelectorAll('.bookSeat');
+// disabling initially
+const inputNumber = document.getElementById("phNumber");
+const nextKey = document.getElementById("next");
+
+disabled(nextKey);
+disabled(inputNumber);
+
+const seats = document.querySelectorAll(".bookSeat");
 
 for (var i = 0; i < seats.length; i++) {
-    const seat = seats[i];
+  const seat = seats[i];
 
-    seat.addEventListener('click', () =>{
-        // get seat number
-        const seatName = seat.innerText;
-        const selectedSeatContainer = document.getElementById('fare');
+  seat.addEventListener("click", () => {
+    // get seat number
+    const seatName = seat.innerText;
+    const selectedSeatContainer = document.getElementById("fare");
 
-        const p1 = document.createElement('p');
-        p1.innerText = seatName;
-        const p2 = document.createElement('p');
-        p2.innerText = busClass;
-        const p3 = document.createElement('p');
-        p3.innerText = price;
+    const p1 = document.createElement("p");
+    p1.innerText = seatName;
+    const p2 = document.createElement("p");
+    p2.innerText = busClass;
+    const p3 = document.createElement("p");
+    p3.innerText = price;
 
-        const div = document.createElement('div');
-        
-        // console.log(div);
-        addFlexStyle(div);
-        flexStyleGrow(p1);
-        flexStyleGrow(p2);
-        flexStyleGrow(p3);
+    const div = document.createElement("div");
 
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
+    // console.log(div);
+    addFlexStyle(div);
+    flexStyleGrow(p1);
+    flexStyleGrow(p2);
+    flexStyleGrow(p3);
 
-        
-        
-        count++; // increment sear selection
-        console.log('count',count);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
 
-        
-        //can only take 4 tickets 
-        if(count <= 4){
-            document.getElementById("bookingCounter").innerText = count;
-            let theSeatCounter = parseInt(document.getElementById("seatCount").innerText);
-            document.getElementById("seatCount").innerText = theSeatCounter - 1;
-            addGreenStyle(seat);
-            // console.log(seat.innerText);
-            selectedSeatContainer.appendChild(div);
-            
-            totalCount("total");
-            totalCount("grandTotal");
+    count++; // increment sear selection
+    console.log("count", count);
 
-            if(count === 4){
-                document.getElementById("couponContainer").classList.remove("hidden");
-                                
-            }
+    //can only take 4 tickets
+    if (count <= 4) {
+      document.getElementById("bookingCounter").innerText = count;
+      let theSeatCounter = parseInt(
+        document.getElementById("seatCount").innerText
+      );
+      document.getElementById("seatCount").innerText = theSeatCounter - 1;
+      addGreenStyle(seat);
+      // console.log(seat.innerText);
+      selectedSeatContainer.appendChild(div);
 
-            // grandTotalCount("grandTotal");
-           
+      totalCount("total");
+      totalCount("grandTotal");
 
-        }else{
-            alert('You can not select more!');
-        }
-        
-    });
+      if (count === 4) {
+        document.getElementById("couponContainer").classList.remove("hidden");
+      }
 
-    function addGreenStyle(e){
-        e.style.backgroundColor = 'green';
-        disabled(e);
+      //phone number next key validation / enable
+
+      if (count >= 1) {
+        enabled(inputNumber);
+        enabled(nextKey);
+
+        console.log(inputNumber);
+        console.log(nextKey);
+
+        inputNumber.addEventListener("keyup", () => {
+          const parseInputNumber = parseInt(inputNumber.value);
+          console.log(parseInputNumber);
+          console.log(count);
+        });
+      }
+      inputNumber.addEventListener("keyup", () => {
+        const parseInputNumber = parseInt(inputNumber.value);
+        console.log(parseInputNumber);
+        console.log(count);
+      });
+
+      // Enabling a disabled button to enable it again
+      // document.getElementById('next').disabled = false;
+
+      // grandTotalCount("grandTotal");
+    } else {
+      alert("You can not select more!");
     }
+  });
+}
 
-    function addFlexStyle(e){
-        e.style.display = 'flex';
-        e.style.flexDirection = 'row';        
-    }
+function addGreenStyle(e) {
+  e.style.backgroundColor = "green";
+  disabled(e);
+}
 
-    function flexStyleGrow(e){        
-        e.style.flexGrow = 1;
-    }
+function addFlexStyle(e) {
+  e.style.display = "flex";
+  e.style.flexDirection = "row";
+}
 
-    function totalCount(e){
-        let totalCount = parseInt(document.getElementById(e).innerText);
-        document.getElementById(e).innerText = totalCount + price;
-    }
+function flexStyleGrow(e) {
+  e.style.flexGrow = 1;
+}
 
-    function disabled(e){
-        e.setAttribute("disabled",true);
-    }
+function totalCount(e) {
+  let totalCount = parseInt(document.getElementById(e).innerText);
+  document.getElementById(e).innerText = totalCount + price;
+}
 
-    function hasCoupon(c){
+function enabled(e) {
+  //   e.setAttribute("disabled", false);
+  e.disabled = false;
+  //   console.log(e);
+}
 
-        const couponCode = document.getElementById(c).value;
-        console.log(couponCode);
+function disabled(e) {
+  e.setAttribute("disabled", true);
+}
 
-        let totalGrandCount = parseInt(document.getElementById("grandTotal").innerText);
-        console.log(totalGrandCount);
-                
-        if(couponCode === 'NEW15'){
-            const discount = totalGrandCount * NEW15;
-            document.getElementById("grandTotal").innerText = totalGrandCount - discount;
-            document.getElementById("couponContainer").classList.add("hidden");
-        }else if(couponCode === 'Couple 20'){
-            const discount = totalGrandCount * Couple20;
-            document.getElementById("grandTotal").innerText = totalGrandCount - discount;
-            document.getElementById("couponContainer").classList.add("hidden");
-        }else{
-            alert('Please give the right coupon!');
-        }
-        
-    }
+function hasCoupon(c) {
+  const couponCode = document.getElementById(c).value;
+  console.log(couponCode);
 
+  let totalGrandCount = parseInt(
+    document.getElementById("grandTotal").innerText
+  );
+  console.log(totalGrandCount);
+
+  if (couponCode === "NEW15") {
+    const discount = totalGrandCount * NEW15;
+    document.getElementById("grandTotal").innerText =
+      totalGrandCount - discount;
+    document.getElementById("couponContainer").classList.add("hidden");
+  } else if (couponCode === "Couple 20") {
+    const discount = totalGrandCount * Couple20;
+    document.getElementById("grandTotal").innerText =
+      totalGrandCount - discount;
+    document.getElementById("couponContainer").classList.add("hidden");
+  } else {
+    alert("Please give the right coupon!");
+  }
 }
